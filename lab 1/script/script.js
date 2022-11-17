c56394fe2ae9251b9866e5765c9c1e7d3264aff2
christrilogy <csm29@sunyorange.edu>
2022-11-17 16:55:40 -0500

    ..
    /

--- lab 1/script/script.js
+++ lab 1/script/script.js
@@ -2,8 +2,8 @@
 function hamburger() {
   var menu = document.getElemntById("menu-links");
   if (menu.style.display === "block") {
-    menu.style.display = "none" ;
-  }else {
+    menu.style.display = "none";
+  } else {
     menu.style.display = "block";
   }
 }
